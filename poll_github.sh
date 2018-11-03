a310547d11bcead9213d081449988a87f9e7c035
loven <loven.francis+isentia@gmail.com>
2018-11-03 16:42:27 +0800

    poll github start

--- poll_github.sh
+++ poll_github.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
+/usr/bin/git remote update
+
 UPSTREAM=${1:-'@{u}'}
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse "$UPSTREAM")
-BASE=$(git merge-base @ "$UPSTREAM")
+LOCAL=$(/usr/bin/git rev-parse @)
+REMOTE=$(/usr/bin/git rev-parse "$UPSTREAM")
+BASE=$(/usr/bin/git merge-base @ "$UPSTREAM")
 
 if [ $LOCAL = $REMOTE ]; then
     echo "Up-to-date"
 elif [ $LOCAL = $BASE ]; then
     echo "Need to pull"
+    /usr/bin/git pull origin develop #TEMP develop to master
 elif [ $REMOTE = $BASE ]; then
     echo "Need to push"
 else
