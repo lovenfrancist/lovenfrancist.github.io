2058892817f836028ee12d94ccb1947c939d121e
loven <loven.francis+isentia@gmail.com>
2018-11-03 17:08:46 +0800

    commit/tag test start

--- poll_github.sh
+++ poll_github.sh
@@ -6,7 +6,6 @@ UPSTREAM=${1:-'@{u}'}
 LOCAL=$(/usr/bin/git rev-parse @)
 REMOTE=$(/usr/bin/git rev-parse "$UPSTREAM")
 BASE=$(/usr/bin/git merge-base @ "$UPSTREAM")
-TAG=$(/usr/bin/git describe @)
 
 if [ $LOCAL = $REMOTE ]; then
     echo "Up-to-date"
@@ -14,6 +13,8 @@ elif [ $LOCAL = $BASE ]; then
     echo "Need to pull"
     /usr/bin/git pull origin develop #TEMP develop to master
     /usr/bin/git fetch --tags
+    TAG=$(/usr/bin/git describe @)
+    echo "TAG:$TAG"
 elif [ $REMOTE = $BASE ]; then
     echo "Need to push"
 else
