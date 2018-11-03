a657147b15177f9ba4504b850d3b147da651f263
loven <loven.francis+isentia@gmail.com>
2018-11-03 09:35:06 +0800

    Add new post script done

--- add_post.py
+++ add_post.py
@@ -1,14 +1,16 @@
 import os
 import datetime
 import subprocess
+from string import Template
 
-post = subprocess.check_output("fortune", shell=True)
-post_title = post.split()[:5]
-post_title = ' '.join(post_title)
-newfile = datetime.datetime.now().strftime("%Y-%m-%d") + "-" + post_title + ".markdown"
+post_body = subprocess.check_output("fortune", shell=True)
+post_title = ' '.join(post_body.split()[:5])
+post_date = datetime.datetime.now().strftime("%Y-%m-%d")
+newfile = post_date + "-" + post_title + ".markdown"
+post_dict={ 'post_title':post_title, 'post_body':post_body, 'post_date':post_date }
 
 with open(os.path.join(os.getcwd(), \
 "templates", "post.j2"), "rt") as post_template:
     with open(os.path.join(os.getcwd(), "_posts", newfile), "wt") as fout:
-        for line in post_template:
-            fout.write(line.replace('{{ post_title }}', post_title))
+        src = Template( post_template.read() )
+        fout.write(src.substitute(post_dict))
