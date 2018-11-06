0f60503fcc28daea61cbafc3a40ef71557844bfe
Ubuntu <ubuntu@ip-172-31-21-5.ap-southeast-1.compute.internal>
2018-11-06 09:18:25 +0000

    New Post -

--- tag.sh
+++ tag.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
-/usr/bin/git tag -a v0.20.85 -m, "v0.20.85"
-/usr/bin/git push origin v0.20.85
+/usr/bin/git remote set-url origin git@github.com:lovenfrancist/lovenfrancist.github.io.git
+eval $(ssh-agent -s) && ssh-add ~/.ssh/id_rsa
+
+/usr/bin/git add -A
+/usr/bin/git commit -m 'New Post - '
+/usr/bin/git push origin develop:develop
+
+/usr/bin/git tag -a v0.22.85 -m, "v0.22.85"
+/usr/bin/git push origin v0.22.85
